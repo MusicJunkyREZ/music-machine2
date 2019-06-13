ca133d1555818aed47942abd5e6a7224ff8f9db0
MusicJunkyREZ <reidzuckerman@gmail.com>
2019-06-13 10:41:27 -0600

    changed port for heroku

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/react-auth-simple", { useNewUrlParser: true }, function(err) {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds237357.mlab.com:37357/heroku_l77l8p1h", { useNewUrlParser: true }, function(err) {
     if (err) throw err;
     console.log(`mongoose connection successful`.yellow);
     app.listen(PORT, (err)=> {
